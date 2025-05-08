413e7d5f74a558700186f7d917c2b01d78c6ea8c
Brett Maytom <brett@brettmaytom.com>
2025-05-08 15:57:03 +1000

    First manual release of hugo generated content from submodule wiki/src/public

---
+++ searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Practiqual®",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Practiqual®",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Practiqual®",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
